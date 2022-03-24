0cf0d58a689d0fcd0566549753e9c28702b6cc45
hernandezalvarezsatwit <hernandezalvarezs@wit.edu>
2022-03-23 20:56:06 -0400

    Validator working

---
+++ lab8_problem2.js
@@ -0,0 +1,44 @@
+const cb = document.querySelector("input[type=checkbox].required");
+//cb.addEventListener("click", isChecked);
+
+const submitButton = document.querySelector("input[type=submit]");
+//submitButton.addEventListener("click", preventDefault);
+
+const form = document.querySelector("#mainForm");
+//form.addEventListener('submit', validateForm);
+
+const reqInput = document.querySelectorAll(".required");
+
+//const ev = submitButton.addEventListener("click", preventDefault);
+
+function preventDefault2(){
+    alert("Form was submitted");
+}
+
+form.addEventListener('submit', (e) => {
+    if (reqInput[0].value === "" || reqInput[0].value == null) {
+        alert("Title is not filled. You can not submit");
+        e.preventDefault();
+    }
+    if (reqInput[1].value === "") {
+        alert("Text Area is not filled. You can  not submit");
+        e.preventDefault();
+    }
+    //Check if checkbox is checked
+    if (!reqInput[2].checked) {
+        alert("Checkbox is not checked. You can not submit");
+        e.preventDefault();
+    }
+});
+
+
+
+
+function isChecked(){
+    if(document.querySelector("input[type=checkbox].required").checked){
+        console.log("checked")
+    }
+    else{
+        console.log("Unchecked")
+    }
+}
