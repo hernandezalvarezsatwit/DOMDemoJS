ce9b9f594b45111c949b326d972903a5d969813b
hernandezalvarezsatwit <hernandezalvarezs@wit.edu>
2022-03-23 21:24:32 -0400

    Working

--- lab8_problem2.js
+++ lab8_problem2.js
@@ -1,31 +1,23 @@
-const cb = document.querySelector("input[type=checkbox].required");
 const form = document.querySelector("#mainForm");
 const reqInput = document.querySelectorAll(".required");
-//cb.addEventListener("click", isChecked);
-
 form.addEventListener('submit', validateForm);
 
 function validateForm(e) {
+    //Check if title is blank
     if (reqInput[0].value === "" || reqInput[0].value == null) {
-        alert("Title is not filled. You can not submit");
+        reqInput[0].parentNode.classList.add("highlight");      //Add to parent
+        reqInput[0].classList.add("highlight");                 //Add to child to so that both get colored
         e.preventDefault();
     }
-    if (reqInput[1].value === "") {
-        alert("Text Area is not filled. You can  not submit");
+    //Check if description is blank
+    if (reqInput[1].value === "" || reqInput[1].value == null) {
+        reqInput[1].parentNode.classList.add("highlight");      //Add to parent
+        reqInput[1].classList.add("highlight");                 //Add to child to so that both get colored
         e.preventDefault();
     }
     //Check if checkbox is checked
-    if (!reqInput[2].checked) {
-        alert("Checkbox is not checked. You can not submit");
+    if (!reqInput[2].checked || reqInput[2].value == null) {
+        reqInput[2].parentNode.classList.add("highlight");     //Make surrounding box red
         e.preventDefault();
-    }
-}
-
-function isChecked(){
-    if(document.querySelector("input[type=checkbox].required").checked){
-        console.log("checked")
-    }
-    else{
-        console.log("Unchecked")
     }
 }
