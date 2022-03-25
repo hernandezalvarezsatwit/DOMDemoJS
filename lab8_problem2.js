65006f26e4d9c0fe5c3e0697202e64ca6a1f2c2d
hernandezalvarezsatwit <hernandezalvarezs@wit.edu>
2022-03-25 10:59:09 -0400

    Minor sytle correction

--- lab8_problem2.js
+++ lab8_problem2.js
@@ -1,4 +1,4 @@
-    document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
 
     //Get form and required fields
     const form = document.querySelector("#mainForm");
